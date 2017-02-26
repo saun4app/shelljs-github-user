d5774cf517d25caa5972d13357d100199f6f3449
Saun Shewanown <saun4app@gmail.com>
2017-02-26 11:35:05 -0500

    Change coding style and eslint rules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // @flow
+import shelljs from 'shelljs';
 
 /**
  * promise_github_user .
@@ -6,33 +7,35 @@
  */
 // promise_github_user().then((github_info_obj) => { console.log(github_info_obj) });
 
-const promise_github_user = function() {
-    const promise_obj = new Promise((resolve, reject) => {
-        try {
-            shelljs.exec('git config -l ', { silent: true }, function(code, stdout, stderr) {
-                if (0 == code) {
-                    resolve(_get_github_user(stdout));
-                } else { reject({ 'error': code, 'stderr': stderr }) }
-            });
-        } catch (err) { reject({ 'error': err }) }
-    });
-
-    return promise_obj;
-
-    function _get_github_user(result_str) {
-        var result_str_array = result_str.split('\n');
-        var result_item = {};
-
-        str_array.forEach((line_str) => {
-            var line_array = line_str.split('=');
-            if (line_array.length > 1 && line_array[0].includes('user.')) {
-                var key = line_array[0].replace('user.', '');
-                result_item[key] = line_array[1];
-            }
+class PromiseGithubUser {
+    static promise_github_user() {
+        const promise_obj = new Promise((resolve, reject) => {
+            try {
+                shelljs.exec('git config -l ', { silent: true }, (code, stdout, stderr) => {
+                    if (0 === code) {
+                        resolve(_get_github_user(stdout));
+                    } else { reject({ 'error': code, 'stderr': stderr }) }
+                });
+            } catch (err) { reject({ 'error': err }) }
         });
 
-        return result_item;
+        return promise_obj;
+
+        function _get_github_user(result_str) {
+            const str_array = result_str.split('\n');
+            const result_item = {};
+
+            str_array.forEach((line_str) => {
+                const line_array = line_str.split('=');
+                if (line_array.length > 1 && line_array[0].includes('user.')) {
+                    const key = line_array[0].replace('user.', '');
+                    result_item[key] = line_array[1];
+                }
+            });
+
+            return result_item;
+        }
     }
 }
 
-export default promise_github_user
+export default PromiseGithubUser.promise_github_user;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
-import github_user from '../src'
+import github_user from '../src/index';
 
 describe('promise_github_user', () => {
     it('returns github_obj.name', () => {
         github_user().then((github_obj) => {
-            const _result = typeof(github_obj.name) === 'string';
-            expect(_result).toBe(true)
+            expect(_is_valid(github_obj)).toBe(true);
         });
-    })
+
+        function _is_valid(github_obj) {
+            let valid_result = true;
+            valid_result = !(typeof (github_obj.name) === 'string') ? false : valid_result;
+            valid_result = !((github_obj.name).length >= 2) ? false : valid_result;
+
+            return valid_result;
+        }
+    });
 });
